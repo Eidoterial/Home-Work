0f2dfa9e715716cd97a18ea9597c4974b5493d64
Eidoterial <Eidoterial.p@gmail.com>
2024-02-08 18:39:26 +0200

    Друге збереження
    Закінчено ввід даних та перевірка даних

--- Check_user_data.py
+++ Check_user_data.py
@@ -1,2 +1,38 @@
+today_year = 2024
+today_month = 2
+
+def check_user_name(name: str):
+
+    if name.isalpha(): return True
+
+    else: return False
 
 
+def check_user_month(month: int):
+
+    if 0 < month <= 12: return True
+
+    else: return False
+
+
+def check_user_year(year: int):
+
+    if 0 < year <= 2024: return True
+
+    else: return False
+
+
+def test_born_date(month: int, year: int):
+
+    tot_year = today_year - year
+    tot_month = today_month - month
+
+    if tot_month < 0:
+        tot_month = 12 - abs(tot_month)
+        tot_year -= 1
+
+    elif tot_month == 0: tot_month = 1
+
+    if 5 <= tot_year <= 119: return True
+
+    else: return False
--- User_data.py
+++ User_data.py
@@ -1,4 +1,5 @@
 import Suport_func
+import Check_user_data as Chud
 
 class UserData:
 
@@ -15,14 +16,79 @@ class UserData:
 
     def get_user_input(self):
 
-        test_user_month = int
-        test_user_year = int
-
         if not self.corect_user_name:
 
             while True:
                 print("Впишіть своє ім'я")
-                test_user_name = input("--- >")
+                test_name = input("--- >")
+                Suport_func.clear()
+                if Chud.check_user_name(test_name): break
 
+                else:
+                    print("Ім'я введено некоректно")
+                    Suport_func.sleep(1)
+                    Suport_func.clear()
+
+            self.user_name = test_name
+            self.corect_user_name = True
+
+        if not self.corect_user_month:
+
+            while True:
+
+                test_month = Suport_func.str_blocker("Впишіть ваш номер місяця народження",
+                                                     "Приклад: 6; 3; 11",
+                                                     "Дані вписані коректно",
+                                                     True,
+                                                     True )
+
+                if Chud.check_user_month(test_month): break
+
+                else:
+                    print("Місяць введено некоректно")
+                    Suport_func.sleep(1)
+                    Suport_func.clear()
+
+            self.user_month = test_month
+
+        if not self.corect_user_year:
+
+            while True:
+
+                test_year = Suport_func.str_blocker("Впишіть ваш рік народження",
+                                                     "Приклад: 2000; 1987; 2011",
+                                                     "Дані вписані коректно",
+                                                     True,
+                                                     True)
+
+                if Chud.check_user_year(test_year): break
+
+                else:
+                    print("Рік введено некоректно")
+                    Suport_func.sleep(1)
+                    Suport_func.clear()
+
+            self.user_year = test_year
+
+
+        if Chud.test_born_date(self.user_month, self.user_year):
+            print("Дані зареєстровано")
+
+            self.corect_user_month = True
+            self.corect_user_year = True
+            Suport_func.sleep(1)
+            Suport_func.clear()
+
+            return True
+
+        else:
+
+            print("Ви не входите в віковий діапазон")
+            print("Спробуйте ще раз")
+
+            Suport_func.sleep(1)
+            Suport_func.clear()
+
+            return False
 
 
