9fb17976586970d6ad7677fb7915180dfe6fe62f
Eidoterial <Eidoterial.p@gmail.com>
2024-03-10 10:49:20 +0200

    Clear project

--- Interface.py
+++ Interface.py
@@ -2,60 +2,22 @@ import tkinter
 
 
 class Variables:
-
     def __init__(self):
-        self.select_number_input = tkinter.StringVar()
-        self.result_calculate = tkinter.StringVar(value="0")
+        pass
 
 class Buttons:
-    def __init__(self, input_number_frame, func, var, input_operation_frame, var2, funct2):
-        self.numbers_buttons = []
-        self.operators = []
-        self.numbers_buttons.append(tkinter.Button(input_number_frame, text=f"{1}", width=6, height=1, command=lambda :func(var, str(1))))
-        self.numbers_buttons.append(
-            tkinter.Button(input_number_frame, text=f"{2}", width=6, height=1, command=lambda: func(var, str(2))))
-        self.numbers_buttons.append(
-            tkinter.Button(input_number_frame, text=f"{3}", width=6, height=1, command=lambda: func(var, str(3))))
-        self.numbers_buttons.append(
-            tkinter.Button(input_number_frame, text=f"{4}", width=6, height=1, command=lambda: func(var, str(4))))
-        self.numbers_buttons.append(
-            tkinter.Button(input_number_frame, text=f"{5}", width=6, height=1, command=lambda: func(var, str(5))))
-        self.numbers_buttons.append(
-            tkinter.Button(input_number_frame, text=f"{6}", width=6, height=1, command=lambda: func(var, str(6))))
-        self.numbers_buttons.append(
-            tkinter.Button(input_number_frame, text=f"{7}", width=6, height=1, command=lambda: func(var, str(7))))
-        self.numbers_buttons.append(
-            tkinter.Button(input_number_frame, text=f"{8}", width=6, height=1, command=lambda: func(var, str(8))))
-        self.numbers_buttons.append(
-            tkinter.Button(input_number_frame, text=f"{9}", width=6, height=1, command=lambda: func(var, str(9))))
-        self.numbers_buttons.append(
-            tkinter.Button(input_number_frame, text=f"{0}", width=6, height=1,command=lambda: func(var, str(0))))
-
-        self.operators.append(tkinter.Button(input_operation_frame, text="+", width=6, height=1,
-                              command=lambda: funct2(var, var2, "+")))
-        self.operators.append(tkinter.Button(input_operation_frame, text="-", width=6, height=1,
-                              command=lambda: funct2(var, var2, "-")))
-        self.operators.append(tkinter.Button(input_operation_frame, text="*", width=6, height=1,
-                              command=lambda: funct2(var, var2, "*")))
-        self.operators.append(tkinter.Button(input_operation_frame, text="/", width=6, height=1,
-                              command=lambda: funct2(var, var2, "/")))
-        self.operators.append(tkinter.Button(input_operation_frame, text="=", width=6, height=1,
-                              command=lambda: funct2(var, var2, "=")))
-        self.operators.append(tkinter.Button(input_operation_frame, text="R", width=6, height=1,
-                                             command=lambda: funct2(var, var2, "R")))
+    def __init__(self):
+        pass
 
 class Entrys:
-    def __init__(self, calculate_main_display_frame, select_number_input):
-        self.display_calculate = tkinter.Entry(calculate_main_display_frame, width=10, font=(None, 30), textvariable=select_number_input)
+    def __init__(self):
+        pass
 
 
 class Labeles:
-    def __init__(self, fraim_random_num_generate, fraim_user_input_number):
+    def __init__(self):
         pass
 
 class Frames:
-    def __init__(self, main_display):
-        self.calculate_main_display_frame = tkinter.Frame(main_display, background="red")
-        self.zone_input_calculate_frame = tkinter.Frame(main_display, background="blue")
-        self.input_number_frame = tkinter.Frame(self.zone_input_calculate_frame, background="green")
-        self.input_operation_frame = tkinter.Frame(self.zone_input_calculate_frame, background="cyan")
+    def __init__(self):
+        pass
--- Program.py
+++ Program.py
@@ -3,57 +3,17 @@ import Interface as In_f
 import tkinter
 
 
-def exits():
-    program.main_display.quit()
-
-
-
 
 class Program:
 
     def __init__(self):
         self.main_display = tkinter.Tk()
         self.main_display.geometry("400x450")
+        self.main_display.title("Конвентор валют")
 
-        self.variables = In_f.Variables()
-
-        self.functions1 = Func.input_number
-        self.functions2 = Func.operation
-
-        self.frames = In_f.Frames(self.main_display)
-
-        self.buttons = In_f.Buttons(self.frames.input_number_frame, self.functions1, self.variables.select_number_input, self.frames.input_operation_frame, self.variables.result_calculate, self.functions2)
-
-        self.entrys = In_f.Entrys(self.frames.calculate_main_display_frame, self.variables.select_number_input)
 
 
     def run(self):
-        self.frames.calculate_main_display_frame.place(relx=0, rely=0, relwidth=1, relheight=0.2)
-        self.entrys.display_calculate.place(relx=0.05, rely=0.05, relwidth=0.9, relheight=0.9)
-
-
-        self.frames.zone_input_calculate_frame.place(relx=0, rely=0.2, relwidth=1, relheight=0.8)
-        self.frames.input_number_frame.place(relx=0.05, rely=0.1, relwidth=0.55, relheight=0.8)
-
-        ibnl = 0
-        for r in range(3):
-            for c in range(3):
-                self.buttons.numbers_buttons[ibnl].grid(row=r, column=c, padx=10, pady=23)
-                ibnl += 1
-
-        self.buttons.numbers_buttons[ibnl].grid(row=4, column=1, padx=10, pady=23)
-
-
-        self.frames.input_operation_frame.place(relx=0.65, rely=0.1, relwidth=0.3, relheight=0.8)
-
-        ibnl = 0
-        for r in range(2):
-            for c in range(2):
-                self.buttons.operators[ibnl].grid(row=r, column=c, padx=5, pady=25)
-                ibnl += 1
-
-        self.buttons.operators[ibnl].grid(row=3, column=0, padx=5, pady=23)
-        self.buttons.operators[ibnl + 1].grid(row=3, column=1, padx=5, pady=23)
 
         self.main_display.mainloop()
 
